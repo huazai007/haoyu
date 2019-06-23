3316e0451cc8d26053f2c6dbc25a652b5e022f3a
zhangsh <zhangsh@91power.com>
2019-06-23 17:40:22 +0800

    int

--- day10/杨辉三角.py
+++ day10/杨辉三角.py
@@ -1,1 +1,25 @@
+'''
+def triangles():
+	b = [1]
+	while True:
+		yield b
+		b = [1] + [x + b[i + 1] for i, x in enumerate(b[:-1])] + [1]
 
+n = 0
+results = []
+for t in triangles():
+    print(t)
+    results.append(t)
+    n = n + 1
+    if n == 10:
+        break
+   '''
+
+b=[1,2,1]
+b=[1] + [x + b[i + 1] for i, x in enumerate(b[:-1])] + [1]
+print(b)
+'''
+b=['aa','bb','cc']
+for i,v in enumerate(b[:-1]):
+    print(i,v)
+    '''
