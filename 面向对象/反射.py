987be5af7d68b7a90d7ed4a588a4260f0527586f
zhangsh <zhangsh@91power.com>
2019-06-25 14:46:49 +0800

    init

---
+++ 面向对象/反射.py
@@ -0,0 +1,32 @@
+#类  静态属性 类方法 静态方法
+#命名空间.XXX == getattr(命名空间,'XXX')
+class Student:
+    ROLE = 'STUDENT'
+    def __init__(self,name):
+        self.name=name
+
+    @classmethod
+    def check_course(cls):
+        print('查看课程了')
+
+    @staticmethod
+    def login():
+        print('登录')
+    def hehe(self):
+        print("你好,%s" %(self.name))
+# 反射查看属性
+# print(Student.ROLE)
+# print(getattr(Student,'ROLE'))
+
+# 反射调用方法
+# getattr(Student,'check_course')()  # 类方法
+# getattr(Student,'login')()         # 静态方法
+
+s1=Student("haoyu")
+
+num = input('num:>>> ')
+
+if hasattr(s1,num):
+    #getattr(Student,num)()
+    getattr(s1,num)()
+
